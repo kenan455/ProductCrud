58d2062f92c64c342c87d0a4b48cbd9115387405
Kenan Fintelman <kenanfintelman123@hotmail.com>
2021-03-22 17:01:05 -0300

    Initial commit

---
+++ resources/views/list-produtos.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CRUD com Laravel</title>
+        
+        <!-- Favicon -->
+        <link href="{{URL::asset('img/favicon.ico')}}" rel="shortcut icon">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
+
+        <!-- Styles -->
+        
+        <link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" /> 
+        <link href="{{URL::asset('css/lightbox.css')}}" rel="stylesheet" type="text/css" /> 
+        <link rel="stylesheet" type="text/css" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+
+        <!-- JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="{{URL::asset('js/ajax.js')}}"></script>
+        <script src="{{URL::asset('js/lightbox.js')}}"></script>
+    </head>
+    <body>
+
+        @if (session('message'))
+        <div class="alert alert-success alert-dismissible">
+            <a href="#" class="close" 
+               data-dismiss="alert"
+               aria-label="close">&times;</a>
+            {{ session('message') }}
+        </div>
+        @endif
+        <div id="line-one">   
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" id="center"> 
+                        <h1><b>Produtos</b></h1>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <br>
+                        <a href="{{route('product.create')}}" 
+                           class="btn btn-default btn-sm pull-right">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                            </svg>
+                            <span class="glyphicon glyphicon-plus"></span> Adicionar</a>
+                            </form>
+                        </div>
+                    </div>           
+                </div>
+                <div class="row">
+                    <div class="col-md-12">   
+                        <br />
+                        <h4 id="center"><b>PRODUTOS CADASTRADOS ({{$total}})</b></h4>
+                        <br>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th id="center">Código</th>
+                                        <th id="">Nome</th>
+                                        <th id="">Descrição</th>
+                                        <th id="center">Quantidade</th>
+                                        <th id="">Preço</th>                                    
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($produtos as $produto)
+                                    <tr>
+                                        <td id="center">{{$produto->id}}</td>
+                                        <td title="Nome">{{$produto->name}}</td>
+                                        <td title="Descrição">{{$produto->description}}</td>
+                                        <td title="Quantidade" id="center">{{$produto->quantity}}</td>
+                                        <td title="Preço">R$ {{number_format($produto->price, 2,',','.')}}</td> 
+                                        
+                                        <td id="center">
+                                            <a href="{{route('product.edit', $produto->id)}}" 
+                                               data-toggle="tooltip" 
+                                               data-placement="top"
+                                               title="Alterar"><i class="fa fa-pencil"></i></a>
+                                            &nbsp;<form style="display: inline-block;" method="POST" 
+                                                        action="{{route('product.destroy', $produto->id)}}"                                                        
+                                                        data-toggle="tooltip" data-placement="top"
+                                                        title="Excluir" 
+                                                        onsubmit="return confirm('Confirma exclusão?')">
+                                                {{method_field('DELETE')}}{{ csrf_field() }}                                                
+                                                <button type="submit" style="background-color: #fff">
+                                                    <a><i class="fa fa-trash-o"></i></a>                                                    
+                                                </button></form></td>               
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="{{URL::asset('img/subir.png')}}" 
+                 id="up" 
+                 style="display: none;" 
+                 alt="Ícone Subir ao Topo" 
+                 title="Subir ao topo?">
+            </body>
+            </html>
+
+
